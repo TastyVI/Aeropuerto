7c15e36fae00102299358a8a541e82f053267797
Darlo GD <carloslwalkerjoin@gmail.com>
2024-05-07 12:34:32 -0600

    otra prueva

--- Aero_Formularios/Mapa_de_Mexico.Designer.cs
+++ Aero_Formularios/Mapa_de_Mexico.Designer.cs
@@ -29,22 +29,33 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(398, 121);
+            button1.Location = new Point(77, 91);
             button1.Name = "button1";
             button1.Size = new Size(143, 214);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.Location = new Point(453, 78);
+            button2.Name = "button2";
+            button2.Size = new Size(162, 297);
+            button2.TabIndex = 1;
+            button2.Text = "movimientto naranja";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Mapa_de_Mexico
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Mapa_de_Mexico";
             Text = "Mapa_de_Mexico";
@@ -54,5 +65,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
